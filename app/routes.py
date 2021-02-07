45a439915f274e15edcf2c8e29453cd8502d69e4
unknown <avinashupadhya99@gmail.com>
2021-02-07 22:25:52 +0530

    Moved templates

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,19 @@ from flask import Flask, flash, request, redirect, url_for, render_template
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files.get('file')
+        if not file:
+            return
+        img_bytes = file.read()
+        class_id, class_name = get_prediction(image_bytes=img_bytes)
+        class_name = format_class_name(class_name)
+        return render_template('result.html', class_id=class_id,
+                               class_name=class_name)
+    return render_template('index.html')
